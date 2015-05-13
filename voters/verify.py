d8ba29d02e7a3972520171ad040f8985b53313e5
Changping Chen <ccp0101@gmail.com>
2015-05-12 23:47:37 -0400

    complete verification

--- voters/verify.py
+++ voters/verify.py
@@ -1,11 +1,11 @@
 import sys, os.path
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
-from common.base import SignedStructure
+from common.base import SignedStructure, Commitment
+import base64
 
 def verify_encounter(encounter):
     # returns (verification result, winner, msg)
     challenger, defender = encounter.challenger, encounter.defender
-    moves = encounter.moves.split("#")
 
     move_types = ['CommitTransaction', 'CommitTransaction',
         'RevealTransaction', 'RevealTransaction', 'CommitTransaction',
@@ -13,9 +13,12 @@ def verify_encounter(encounter):
         'CommitTransaction', 'CommitTransaction', 'RevealTransaction',
         'RevealTransaction', 'Resolution']
 
-
-    for idx, encoded_move in enumerate(moves):
+    moves = []
+    signed_moves = []
+    for idx, encoded_move in enumerate(encounter.moves.split("#")):
         move = SignedStructure.deserialize(encoded_move)
+        signed_moves.append(move)
+        moves.append(move.payload)
 
         # Received move must be signed by either defender
         # or challenger. If it is not, the poster should
@@ -30,16 +33,67 @@ def verify_encounter(encounter):
                 return (False, None, msg)
 
         if idx > 1:
-            prev_move = SignedStructure.deserialize(moves[idx - 1])
-            if prev_move.signature != move.payload.prev:
+            if signed_moves[idx - 1].signature != move.payload.prev:
                 msg = "Move #%i does not have correct previous signature." % idx
                 return (False, None, msg)
 
         if move.payload.name != move_types[idx]:
             msg = "Move #%i should be %s." % (idx, move_types[idx])
-                return (False, None, msg)
+            return (False, None, msg)
 
-    return (True, challenger, None)
+    round_idx = 0
+    def compute_round_result(round_idx):
+        defender_commitment = Commitment.deserialize(moves[round_idx].commitment)
+        challenger_commitment = Commitment.deserialize(moves[round_idx+1].commitment)
+        defender_reveal = moves[round_idx+2]
+        challenger_reveal = moves[round_idx+3]
+
+        if not defender_commitment.verifyCommitment(base64.b64decode(defender_reveal.secret),
+            defender_reveal.value):
+            msg = "Defender's commitment verification failed."
+            return (False, None, msg)
+
+        if not challenger_commitment.verifyCommitment(base64.b64decode(challenger_reveal.secret),
+            challenger_reveal.value):
+            msg = "Challenger's commitment verification failed."
+            return (False, None, msg)
+
+        defender_move = defender_reveal.value
+        challenger_move = challenger_reveal.value
+
+        assert defender_move in ["R", "P", "S"]
+        assert challenger_move in ["R", "P", "S"]
+
+        if ((defender_move == "P" and challenger_move == "R") or
+            (defender_move == "R" and challenger_move == "S") or
+            (defender_move == "S" and challenger_move == "P")):
+            return (1, 0)
+        elif defender_move == challenger_move:
+            return (0, 0)
+        else:
+            return (0, 1)
+
+    r1 = compute_round_result(0)
+    if r1[0] is False:
+        return r1
+    r2 = compute_round_result(4)
+    if r2[0] is False:
+        return r2
+    r3 = compute_round_result(8)
+    if r3[0] is False:
+        return r3
+
+    result = (r1[0] + r2[0] + r3[0], r1[1] + r2[1] + r3[1])
+
+    if result[0] == result[1]:
+        return (True, None, "tie")
+
+    if result[0] > result[1]:
+        return (True, defender, "defender wins.")
+
+    if result[0] < result[1]:
+        return (True, challenger, "challenger wins.")
+
 
 if __name__ == "__main__":
     f = open("encounter.p", "rb")
