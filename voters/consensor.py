d1893983cfb1bde200dcded3d90620c76a0e1cdf
jatchili <jecatripple@gmail.com>
2015-05-14 18:27:03 -0400

    cleanup consensor

--- voters/consensor.py
+++ voters/consensor.py
@@ -7,9 +7,11 @@ from common.base import *
 import hashlib
 from verify import verify_encounter, verify_initiation
 
+
 ROUND_TIME = 1500 # milliseconds
 VOTING_THRESHOLDS = [0, 0, 0.5, 0.6, 0.7, 0.8]
 FINAL_THRESHOLD = 0.8
+
 NUMBER_OF_ROUNDS = len(VOTING_THRESHOLDS)
 TRANSACTION_FEE = ledger.TRANSACTION_FEE
 
@@ -48,7 +50,7 @@ class Consensor:
                 transaction_map[tx.id] = tx
                 tally[tx.id] = our_stake
 
-            # ...and then count the others:
+            # ...and then count the others
             for voter in self.votes_heard:
                 their_candidate_set = self.votes_heard[voter]
                 them = self.last_closed_ledger.get_account_info(voter)
@@ -65,10 +67,8 @@ class Consensor:
             # See which ones passed the threshold
             absolute_threshold = int(total_stake * threshold)
             new_candidate_set = set()
-            #logging.info("absolute_threshold:"+str(absolute_threshold))
             for txid in tally:
                 tx = transaction_map[txid]
-                #logging.info(tx.short_id() + " has tally "+str(tally[txid]))
                 if tally[txid] >= absolute_threshold:
                     logging.info("Adding to candidate set tx "+tx.short_id())
                     new_candidate_set.add(tx)
@@ -78,36 +78,24 @@ class Consensor:
             self.candidate_set.clear()
             for t in new_candidate_set: self.candidate_set.add(t)
 
+        # Submit my vote, or apply the final result.
         if self.round_number == NUMBER_OF_ROUNDS-1:
-            self.finalize_ledger(threshold)
+            self.finalize_ledger()
         else:
-            # submit my vote
             self.send_voter_message({
                 "proposal": pickle.dumps(self.candidate_set),
                 "lcl_hash": self.last_closed_ledger.get_ledger_hash()
             })
             self.round_number += 1
-            #logging.info("SENT VOTE")
-            """
-            import random
-            val = "Hello world!"+str(random.randint(0, 10000000))
-            logging.info("SENDING VOTER_MESSAGE:"+val)
-            self.send_voter_message({
-                "payload": val
-            })
-            """
-            
 
-
-    def finalize_ledger(self, threshold):
-        #return
+    # Do this with all transactions that have reached the maximum threshold
+    def finalize_ledger(self):
         final_proposal = set()
-        #deferrals = set()
         total_fees = 0
         for tx in self.candidate_set:
             final_proposal.add(tx)
             if str(tx.__class__) != "consensor.CoinstakeSummary":
-                total_fees += (2*TRANSACTION_FEE) #Don't include Coinstakes
+                total_fees += (2*TRANSACTION_FEE) #Don't include Coinstakes, since they don't pay fees
         
         # For each voter that we saw, defer a Coinstake transaction
         # so that they get their share of the transaction fees.
@@ -122,25 +110,7 @@ class Consensor:
                 )
                 self.deferral_set.add(css)
 
-
-
-
-        """
-        rewardable_voters = list()
-        rewardable_stake = 0
-        for voter in self.voters_seen:
-            if True: #If voter deserves reward:
-                stake = self.voters_seen[voter]
-                rewardable_voters.append({
-                    "voter_id": voter,
-                    "stake": stake,
-                })
-                rewardable_stake += stake
-
-        # Determine reward by the "House of Representatives" method, breaking ties alphabetically
-        transaction_count = len(final_proposal) # TODO: exclude coinstake transactions
-        """
-
+        # Apply the transactions!
         self.last_closed_ledger.apply_transactions(final_proposal)
 
         # This round's deferrals become the next round's candidates, provided that they're still valid.
@@ -164,17 +134,15 @@ class Consensor:
         self.deferral_set.clear()
         self.votes_heard.clear()
 
-        
-
         logging.info("Done with consensus round")
         self.start()
 
-    # Add transaction to the candidate set, and record my own vote on it (if it's not already in the candidate set)
+    # Add a transaction to the candidate set, and record our own vote on it (if it's not already in the candidate set)
     def consider_transaction(self, transaction):
         logging.info("Received transaction of type: "+transaction.payload.name)
         if transaction.payload.name == "PostInitiateEncounter":
             verification = verify_initiation(transaction.payload)
-            if verification[0]: #If the transaction is internally valid
+            if verification[0]: # If the transaction is internally valid
                 summary = InitiationSummary(
                     id = str(transaction.signature),
                     challenger = verification[1],
@@ -191,15 +159,14 @@ class Consensor:
                     logging.info("Discarding InitiateEncounter transaction inconsistent with ledger")
         elif transaction.payload.name == "CloseEncounter":
             verification = verify_encounter(transaction.payload)
-            #logging.info("Verification: "+str(verification))
-            if verification[0]: #If the transaction is internally valid
+            if verification[0]: # If the transaction is internally valid
                 summary = EncounterSummary(
                     id = str(transaction.signature),
                     winner = verification[1],
                     loser = verification[2],
                     was_tied = (verification[3] == "It was a tie!")
                 )
-                if self.last_closed_ledger.is_valid_encounter_summary(summary): #If the transaction is externally valid
+                if self.last_closed_ledger.is_valid_encounter_summary(summary): # If the transaction is externally valid
                     logging.info("Adding CloseEncounter with id=" + summary.short_id() + " to candidate set")
                     if summary not in self.candidate_set:
                         self.candidate_set.add(summary)
@@ -209,19 +176,12 @@ class Consensor:
         else:
             logging.info("Discarding transaction of unknown type")
 
+    # The first time we see a transaction, send it on to our peers so that they'll vote on it too
     def relay_secondhand_transaction(self, shtx):
         logging.info("Relaying secondhand")
         self.send_voter_message({
             "secondhand": SignedStructure.serialize(shtx)
         })
-
-
-    """
-    # Update the vote tally based on an incoming proposal
-    def receive_proposal(self, proposal):
-        pass
-        #Update voters_seen and total_stake, if necessary
-    """
 
     # Send a message to the other voters
     def send_voter_message(self, msg):
@@ -229,11 +189,9 @@ class Consensor:
 
     # Whenever we get a message from another voter
     def process_voter_message(self, sender_id, msg):
-        #logging.info("CONSENSOR GOT VOTER_MESSAGE FROM:"+str(sender_id)+": "+str(msg))
         if "proposal" in msg: # It's a vote
             sender_account_id = msg["account_id"]
             their_proposal = pickle.loads(msg["proposal"])
-            #logging.info("RECEIVED VOTE")
             # At this point we should check to make sure that the signature on the message is genuine.
             # Also, check to see that they're talking about the same ledger as we are.
             if self.last_closed_ledger.get_ledger_hash() == msg["lcl_hash"]:
@@ -244,22 +202,15 @@ class Consensor:
             shtx = SignedStructure.deserialize(msg["secondhand"])
             self.consider_transaction(shtx)
         else:
-            #logging.info("CONSENSOR GOT VOTER_MESSAGE FROM:"+str(sender_id)+": "+str(msg))
             logging.info("Could not understand voter message; dropping: "+str(msg))
 
-
+    # Start a consensus round
     def start(self):
         logging.info("Starting new consensus round")
-        #logging.info("STARTING ROUND!"+str(self.votes_heard)+"!!!!"+str(self.deferral_set)+"????"+str(self.candidate_set))
-
-        #self.voters_seen = dict() #key=voter id, value = stake in LCL
         my_info = self.last_closed_ledger.get_account_info(self.account.account_id)
         if not my_info:
             logging.error("Account does not exist: "+self.account.account_id)
             return
-        my_stake = my_info["stake"]
-        #self.voters_seen[self.account.account_id] = my_stake
-        #self.total_stake = my_stake
 
         if self.timer: self.timer.stop()
         self.round_number = 0
@@ -270,10 +221,11 @@ class Consensor:
         if self.timer: self.timer.stop()
 
 
-
+# Helper classes to represent "transaction summaries".
+# These describe how a transaction will affect the ledger, but don't carry any signatures.
 class Summary:
     def __init__(self):
-        pass #Override this
+        pass #Override this in subclasses
     def short_id(self):
         return self.id[1:9] + "..."
     def __hash__(self):
@@ -290,7 +242,6 @@ class InitiationSummary(Summary):
         self.defender = defender
         self.encounter_begin_by = encounter_begin_by
         self.encounter_end_by = encounter_end_by
-
 
 class EncounterSummary(Summary):
     def __init__(self, id, winner, loser, was_tied):
